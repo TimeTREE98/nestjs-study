eb0bcdcbfb4b73f745d06863b9a461b295967a8d
TimeTREE <timetree@kakao.com>
2021-02-16 17:00:04 +0900

    express의 route에 해당하는 controller 학습, sayHello() 생성, service getHi() 생성 학습 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import {AppService} from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController], // URL을 가져오고 함수를 실행. express에서 라우트와 같은 역할
   providers: [AppService],
 })
 export class AppModule {}
